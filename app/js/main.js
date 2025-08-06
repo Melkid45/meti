f58f39da553cf17b2b14c8068d183a4515fa8d2a
Mel <167970274+Melkid45@users.noreply.github.com>
2025-08-06 14:50:23 +0600

    last

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,35 @@ function onEntry(entry) {
         }
     });
 }
+let lastScrollTop = 0
+const lenis = new Lenis({
+    lerp: 0.1, // плавность скролла
+    smoothWheel: true // плавный скролл колесом
+})
+if ($(window).width() > 750) {
+    lenis.on('scroll', ({ scroll }) => {
+        const currentScroll = scroll
+        const header = document.querySelector('.header')
+
+        if (currentScroll > lastScrollTop && currentScroll > 50) {
+            header.classList.add('back')
+        } else {
+            header.classList.remove('back')
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll
+    })
+}
+gsap.ticker.add((time) => {
+    lenis.raf(time * 1000)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+gsap.ticker.lagSmoothing(0);
+
 let options = { threshold: [0.5] };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.element-animation');
@@ -19,18 +48,6 @@ for (let elm of elements) {
     observer.observe(elm);
 }
 
-if ($(window).width() > 750) {
-    let lastScrollTop = 0;
-    window.addEventListener('scroll', function () {
-        let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-        if (currentScroll > lastScrollTop) {
-            document.querySelector('.header').classList.add('back')
-        } else {
-            document.querySelector('.header').classList.remove('back')
-        }
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-    });
-}
 $('.burger').on('click', function (e) {
     $('.header__mobile').toggleClass('show')
     $(this).find('.current').toggleClass('show')
