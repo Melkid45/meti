397365c83986b7a6d15c6bcdf636395227c4fce7
alexorlov <alex@broccolidev.ru>
2025-08-09 23:58:21 +0300

    fixes

--- app/js/modules/abaptation.js
+++ app/js/modules/abaptation.js
@@ -2,14 +2,14 @@ let width = $(window).width()
 if (width <= 750){
     $('.header__body-menu').remove()
     $('.about-p1').remove()
-    $('.difference').text('уровня сетевых')
-    $('.difference2').text('рекламных')
+    // $('.difference').text('уровня сетевых')
+    // $('.difference2').text('рекламных')
     $('.brand').attr('placeholder', 'Имя бренда')
 }else{
     $('.burger').remove()
     $('.about-p2').remove()
-    $('.difference').text('уровня')
-    $('.difference2').text('сетевых рекламных')
+    // $('.difference').text('уровня')
+    // $('.difference2').text('сетевых рекламных')
     $('.header__mobile').remove()
     $('.muteor').remove()
 }
