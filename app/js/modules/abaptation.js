b06dd748acc2d01c750f6c1514531dae09cb060e
Mel <167970274+Melkid45@users.noreply.github.com>
2025-08-30 16:38:26 +0600

    last

--- app/js/modules/abaptation.js
+++ app/js/modules/abaptation.js
@@ -20,3 +20,32 @@ if (width <= 820) {
     $('.muteor').remove()
     $('.splide').remove()
 }
+
+
+if (width <= 820) {
+    document.addEventListener('DOMContentLoaded', function () {
+        const lazyVideos = document.querySelectorAll('video.lazy');
+
+        if ('IntersectionObserver' in window) {
+            const lazyVideoObserver = new IntersectionObserver(function (entries, observer) {
+                entries.forEach(function (video) {
+                    if (video.isIntersecting) {
+                        for (var source in video.target.children) {
+                            var videoSource = video.target.children[source];
+                            if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                                videoSource.src = videoSource.dataset.src;
+                            }
+                        }
+                        video.target.load();
+                        video.target.classList.remove("lazy");
+                        lazyVideoObserver.unobserve(video.target);
+                    }
+                });
+            });
+
+            lazyVideos.forEach(function (lazyVideo) {
+                lazyVideoObserver.observe(lazyVideo);
+            });
+        }
+    });
+}
